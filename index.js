500346b2b3a5d675d418816559c962d736a4faf8
SeHun Shin <sshplendid@gmail.com>
2018-06-12 00:29:22 +0900

    feat: create a simple express server
    express 패키지 의존성 추가
    request 객체 파라미터 전달
    에러 핸들링 처리 기능 추가

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 // index.js
 
-require('./app/index');
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  req.chance = Math.random();
+  next();
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Node.js');
+});
+
+app.get('/random', (req, res) => {
+  res.json({chance: req.chance});
+});
+
+app.get('/error', (req, res) => {
+  throw new Error(`Oops!`);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(`500 Server broken!`);
+});
+
+app.listen(port, err => {
+  if(err) return console.error('Something happened');
+
+  console.log(`Server is running at ${port}`);
+})
